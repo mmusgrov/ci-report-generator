6af59efb63c7e65067eef0dd8ea6b4727a4d7464
gytis <gytis@redhat.com>
2015-07-06 12:24:26 +0100

    Order by timestamp

--- generators/swigReportGenerator.js
+++ generators/swigReportGenerator.js
@@ -21,9 +21,10 @@ function writeToFile(path, content) {
     return deferred.promise;
 }
 
-function addLastFailureDates(failures) {
+function addLastFailureInfo(failures) {
     for (var i = 0; i < failures.length; i++) {
         var lastFailedBuild = _.max(failures[i].builds, "timestamp");
+        failures[i].lastFailureTimestamp = lastFailedBuild.timestamp;
         failures[i].lastFailureDate = new Date(lastFailedBuild.timestamp).toDateString();
         failures[i].isNew = lastFailedBuild.timestamp > (new Date().getTime() - weekMilliseconds);
     }
@@ -31,7 +32,7 @@ function addLastFailureDates(failures) {
     return failures;
 }
 
-function addDateToBuilds(builds) {
+function addBuildInfo(builds) {
     for (var i = 0; i < builds.length; i++) {
         builds[i].date = new Date(builds[i].timestamp).toDateString();
         builds[i].isNew = builds[i].timestamp > (new Date().getTime() - weekMilliseconds);
@@ -40,17 +41,20 @@ function addDateToBuilds(builds) {
     return builds;
 }
 
+function documentedFailuresFilter(failure) {
+    return failure.jiraIssueKey !== null;
+}
+
 module.exports = function(failuresMap) {
     var deferred = q.defer();
     var failuresArray = _.values(failuresMap);
-    var documentedFailures = _.filter(failuresArray, function(failure) {
-        return failure.jiraIssueKey !== null;
-    });
+    var documentedFailures = _.sortBy(addLastFailureInfo(_.filter(failuresArray, documentedFailuresFilter)), "lastFailureTimestamp").reverse();
+    var undocumentedBuilds = _.sortBy(addBuildInfo(failuresMap.other.builds), "timestamp").reverse();
 
     var options = {
         currentDate: new Date().toDateString(),
-        documentedFailures: addLastFailureDates(documentedFailures),
-        undocumentedBuilds: addDateToBuilds(failuresMap.other.builds)
+        documentedFailures: documentedFailures,
+        undocumentedBuilds: undocumentedBuilds
     };
 
     swig.renderFile(templatePath, options, function(error, content) {
